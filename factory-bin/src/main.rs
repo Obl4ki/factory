768622ae726a6aa25048086aedb603560b8feecf
Obl4ki <lukaszoblak33@gmail.com>
2024-02-29 15:56:32 +0100

    Implement generating crafting subtrees, impl Display for Node, fix docs

--- factory-bin/src/main.rs
+++ factory-bin/src/main.rs
@@ -1,5 +1,6 @@
 use factory_lib::{domain::Node, entities::Item, prelude::*};
 
+use petgraph::dot::{Config, Dot};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -8,8 +9,18 @@ enum ApplicationError {
     DataError(#[from] DataError),
 }
 
+#[allow(unused)]
 fn main() -> Result<(), ApplicationError> {
     let data = load_dataset("recipe-lister/recipe.json")?;
     let recipe_graph = CraftingGraph::from_dataset(&data);
+
+    let search_item = &Item {
+        name: "electronic-circuit".to_string(),
+    };
+    
+    let graphs = recipe_graph
+        .get_crafting_trees(Node::Item(search_item))
+        .expect("Result should be present");
+
     Ok(())
 }
--- factory-lib/src/domain.rs
+++ factory-lib/src/domain.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
+use std::fmt;
 
-use crate::entities::Item;
-use crate::entities::ItemAmount;
-use crate::entities::Recipe;
+use crate::entities::{Item, ItemAmount, Recipe};
 
 use petgraph::graph::NodeIndex;
 use petgraph::prelude::*;
 
 #[derive(Debug, Clone)]
 pub struct CraftingGraph<'data> {
-    data: DiGraph<Node<'data>, ItemAmount>,
+    pub data: DiGraph<Node<'data>, ItemAmount>,
 }
 
 impl<'data> From<DiGraph<Node<'data>, ItemAmount>> for CraftingGraph<'data> {
@@ -22,6 +21,21 @@ impl<'data> From<DiGraph<Node<'data>, ItemAmount>> for CraftingGraph<'data> {
 pub enum Node<'a> {
     Item(&'a Item),
     Recipe(&'a Recipe),
+}
+
+impl<'a> fmt::Display for Node<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Node::Item(item) => {
+                f.write_str(&format!("Item: {}", &item.name))?;
+            }
+            Node::Recipe(recipe) => {
+                f.write_str(&format!("Recipe: {}", &recipe.name))?;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 /// Create a directed graph of items and recipes.
@@ -37,6 +51,12 @@ pub enum Node<'a> {
 /// - (Recipe -> Item), then it has a weight that describes how much items can be crafted from this recipe.
 /// - (Item -> Recipe), then it's weight describes the amount of items needed for target recipe.
 impl<'data> CraftingGraph<'data> {
+    pub fn new() -> Self {
+        Self {
+            data: DiGraph::new(),
+        }
+    }
+
     pub fn from_dataset(dataset: &'data [Recipe]) -> Self {
         let mut nodes: HashMap<Node, NodeIndex> = HashMap::new();
         let mut g = DiGraph::new();
@@ -109,9 +129,9 @@ impl<'data> CraftingGraph<'data> {
         }
     }
 
-    /// Get all indices of recipes that use the item provided as ingredient.
+    /// Get all indices of recipes that result in creation of this item.
     /// If the node is not an item or it doesn't exist in graph, None is returned.
-    pub fn get_items_as_outputs_in_recipes_idxs(&self, node: Node) -> Option<Vec<NodeIndex>> {
+    pub fn get_recipes_with_item_in_outputs(&self, node: Node) -> Option<Vec<NodeIndex>> {
         match node {
             Node::Item(_) => Some(
                 self.data
@@ -122,7 +142,6 @@ impl<'data> CraftingGraph<'data> {
         }
     }
 
-
     pub fn get_node_idx(&self, target_node: Node) -> Option<NodeIndex> {
         self.data
             .node_weights()
@@ -130,11 +149,128 @@ impl<'data> CraftingGraph<'data> {
             .map(|raw_idx| NodeIndex::from(raw_idx as u32))
     }
 
-    pub fn get_crafting_tree(&self, target: Node) -> Option<Self> {
-        todo!()
+    /// Starting at the target node, get a list of possible crafting paths an item can have.
+    /// Each time an item can be crafted from multiple (N) recipes, this graph will branch into N graphs, which will be processed
+    /// further, until each crafting tree is complete.
+    /// Passing [`Node::Recipe`] as target will consider the concrete recipe as a starting point, meanwhile
+    /// [`Node::Item`] will consider every recipe which result in this item.
+    /// If target doesn't exist in graph, then None is returned.
+
+    pub fn get_crafting_trees(&'data self, target: Node<'data>) -> Option<Vec<Self>> {
+        let mut complete_subgraphs: Vec<Self> = vec![];
+
+        let target_idx = self.get_node_idx(target)?;
+
+        let mut first_tree: Self = Self::new();
+        let subgraph_head_idx = first_tree.data.add_node(target);
+
+        // Subgraph, indexes of nodes in self, indexes of nodes in subgraph
+        let mut processing_queue: Vec<(Self, Vec<NodeIndex>, Vec<NodeIndex>)> =
+            vec![(first_tree, vec![target_idx], vec![subgraph_head_idx])];
+
+        while let Some((
+            mut subgraph,
+            mut graph_processing_indices,
+            mut subgraph_processing_indices,
+        )) = processing_queue.pop()
+        {
+            if subgraph_processing_indices.is_empty() || graph_processing_indices.is_empty() {
+                complete_subgraphs.push(subgraph);
+                continue;
+            }
+
+            let current_graph_idx = graph_processing_indices.pop()?;
+            let current_subgraph_idx = subgraph_processing_indices.pop()?;
+
+            match subgraph.data[current_subgraph_idx] {
+                Node::Item(_) => {
+                    let recipe_graph_idxs =
+                        self.get_recipes_with_item_in_outputs(self.data[current_graph_idx]);
+
+                    if recipe_graph_idxs.as_ref().is_some_and(Vec::is_empty) {
+                        processing_queue.push((
+                            subgraph,
+                            graph_processing_indices,
+                            subgraph_processing_indices,
+                        ));
+                        continue;
+                    }
+
+                    for recipe_graph_idx in recipe_graph_idxs? {
+                        let recipe = self.data[recipe_graph_idx];
+
+                        let mut branched_subgraph = subgraph.clone();
+
+                        let added_recipe_subgraph_idx = branched_subgraph.data.add_node(recipe);
+
+                        let recipe_output = self
+                            .data
+                            .edges_connecting(recipe_graph_idx, current_graph_idx)
+                            .map(|edge| *edge.weight())
+                            .next()?;
+
+                        branched_subgraph.data.add_edge(
+                            added_recipe_subgraph_idx,
+                            current_subgraph_idx,
+                            recipe_output,
+                        );
+
+                        let mut branched_graph_processing_indices =
+                            graph_processing_indices.clone();
+
+                        branched_graph_processing_indices.push(recipe_graph_idx);
+
+                        let mut branched_subgraph_processing_indices =
+                            subgraph_processing_indices.clone();
+                        branched_subgraph_processing_indices.push(added_recipe_subgraph_idx);
+
+                        processing_queue.push((
+                            branched_subgraph,
+                            branched_graph_processing_indices,
+                            branched_subgraph_processing_indices,
+                        ))
+                    }
+                }
+                Node::Recipe(_) => {
+                    let item_graph_idxs = self.get_recipes_input_idxs(self.data[current_graph_idx]);
+
+                    for item_graph_idx in item_graph_idxs? {
+                        let item = self.data[item_graph_idx];
+
+                        let added_item_subgraph_idx = subgraph.data.add_node(item);
+
+                        subgraph.data.add_edge(
+                            added_item_subgraph_idx,
+                            current_subgraph_idx,
+                            self.data
+                                .edges_connecting(item_graph_idx, current_graph_idx)
+                                .map(|edge| *edge.weight())
+                                .next()?,
+                        );
+
+                        graph_processing_indices.push(item_graph_idx);
+                        subgraph_processing_indices.push(added_item_subgraph_idx);
+                    }
+
+                    processing_queue.push((
+                        subgraph,
+                        graph_processing_indices,
+                        subgraph_processing_indices,
+                    ));
+                }
+            }
+        }
+
+        Some(complete_subgraphs)
     }
 
     pub fn indices_to_nodes(&self, indices: &[NodeIndex]) -> Vec<Node> {
         indices.iter().map(|idx| self.data[*idx]).collect()
+    }
+}
+
+impl<'data> Default for CraftingGraph<'data> {
+    fn default() -> Self {
+        Self::new()
     }
 }
